9aed69125da85f0e8004c8a946cb3ffe55ff9ec2
John <johnloveskittens@mail.com>
2020-03-20 00:17:55 +0800

    Был пределан макет для виджетов ввода

--- screens/variable/LearnAbilityScreen.py
+++ screens/variable/LearnAbilityScreen.py
@@ -20,40 +20,82 @@ class AbilitiesScreen(BaseScreen):
         self.tip = tip
         layout.addWidget(self.tip)
 
-        # Init reading speed layout
-        read_layout = QtWidgets.QHBoxLayout()
-        read_tip = QtWidgets.QLabel()  # Read tip
+        # # Макет для ввода скорости чтения
+        # read_layout = QtWidgets.QHBoxLayout()
+        # read_tip = QtWidgets.QLabel()  # Read tip
+        # read_tip.setObjectName("read_tip")
+        # read_tip.setFont(tipFont)
+        # read_tip.sizePolicy().setHorizontalStretch(1)
+        # read_field = QtWidgets.QLineEdit()  # Read field
+        # read_field.setObjectName("read_field")
+        # read_tip.sizePolicy().setHorizontalStretch(1)
+        # read_field.setMinimumHeight(20)
+        # # Макет для ввода скорости просмотра видео
+        # watch_layout = QtWidgets.QHBoxLayout()
+        # watch_tip = QtWidgets.QLabel()  # Watch tip
+        # watch_tip.setObjectName("watch_tip")
+        # watch_tip.setFont(tipFont)
+        # watch_tip.sizePolicy().setHorizontalStretch(1)
+        # watch_field = QtWidgets.QLineEdit()  # Watch field
+        # watch_field.setObjectName("watch_field")
+        # watch_field.sizePolicy().setHorizontalStretch(1)
+        # watch_field.setMinimumHeight(20)
+        #
+        # # Add widgets in to the layouts
+        # self.read_tip = read_tip
+        # self.read_field = read_field
+        # self.watch_tip = watch_tip
+        # self.watch_field = watch_field
+        # read_layout.addWidget(self.read_tip)
+        # read_layout.addSpacing(10)
+        # read_layout.addWidget(self.read_field)
+        # read_layout.addStretch(2)
+        # watch_layout.addWidget(self.watch_tip)
+        # watch_layout.addSpacing(10)
+        # watch_layout.addWidget(self.watch_field)
+        # watch_layout.addStretch(2)
+        # layout.addSpacing(10)
+        # layout.addLayout(read_layout)
+        # layout.addSpacing(2)
+        # layout.addLayout(watch_layout)
+
+        # Создание макета для ввода скоростей чтения и просмотра видео
+        speed_layout = QtWidgets.QGridLayout()  # Сеточный макет в котором мы расположим виджеты для ввода
+        speed_layout.setSpacing(3)  # Установка внутренних отступов макета
+
+        read_tip = QtWidgets.QLabel()  # Подсказка для поля ввода скорости чтения
         read_tip.setObjectName("read_tip")
         read_tip.setFont(tipFont)
-        read_field = QtWidgets.QLineEdit()  # Read field
-        read_field.setObjectName("read_field")
-        read_field.setSizePolicy(fieldPolicy)
-        read_field.setMinimumHeight(20)
-        # Init watching speed layout
-        watch_layout = QtWidgets.QHBoxLayout()
-        watch_tip = QtWidgets.QLabel()  # Watch tip
+        watch_tip = QtWidgets.QLabel()  # Подсказка для поля ввода скорости просмотра видео
         watch_tip.setObjectName("watch_tip")
         watch_tip.setFont(tipFont)
-        watch_field = QtWidgets.QLineEdit()  # Watch field
+
+        read_field = QtWidgets.QLineEdit()  # Поле ввода скорости чтения
+        read_field.setObjectName("read_field")
+        read_field.setMinimumHeight(20)
+        watch_field = QtWidgets.QLineEdit()  # Поле ввода скорости просмотра видео
         watch_field.setObjectName("watch_field")
-        watch_field.setSizePolicy(fieldPolicy)
         watch_field.setMinimumHeight(20)
 
-        # Add widgets in to the layouts
+        # Установка пременных в качестве аттрибутов-данных объекта
+        # Это для того, чтобы к этим виджетам был доступ извне
         self.read_tip = read_tip
         self.read_field = read_field
         self.watch_tip = watch_tip
         self.watch_field = watch_field
-        read_layout.addWidget(self.read_tip)
-        read_layout.addWidget(self.read_field)
-        watch_layout.addWidget(self.watch_tip)
-        watch_layout.addWidget(self.watch_field)
-        layout.addSpacing(10)
-        layout.addLayout(read_layout)
-        layout.addSpacing(2)
-        layout.addLayout(watch_layout)
 
-        layout.addStretch(2)
+        # Добавлнеие поле ввода для чтения
+        speed_layout.addWidget(self.read_tip, 1, 0)
+        speed_layout.addWidget(self.read_field, 1, 1)
+        # Добавлнеие поле ввода для видео
+        speed_layout.addWidget(self.watch_tip, 2, 0)
+        speed_layout.addWidget(self.watch_field, 2, 1)
+
+        # Установка макета с полями ввода в основной макет
+        layout.addSpacing(10)
+        layout.addLayout(speed_layout)
+
+        layout.addStretch(1)
 
     def translate_text(self, main_window):
         _translate = QtCore.QCoreApplication.translate
